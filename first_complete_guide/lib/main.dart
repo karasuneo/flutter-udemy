1b764dfbfffbb343bc40c1ef8fdddcaab5087aa1
Yusei Mizutani <yusei4520030401@gmail.com>
2023-01-26 23:10:51 +0900

    ボタンの表示

--- first_complete_guide/lib/main.dart
+++ first_complete_guide/lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
+
+void answerQuestion() {
+  print("Answer choosen");
+}
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      "what is your favorite color?",
+      "What is your favorite animal?",
+    ];
     return MaterialApp(
-      home: Text('HelloWorld'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("My First App"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text("This question!"),
+              ElevatedButton(child: Text("Answer1"), onPressed: answerQuestion),
+              ElevatedButton(child: Text("Answer2"), onPressed: answerQuestion),
+              ElevatedButton(child: Text("Answer3"), onPressed: answerQuestion),
+            ],
+          )),
     );
   }
 }
