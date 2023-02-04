c8d4552b53ff510a9d43dad2b396ec1895ca5b66
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-04 22:43:50 +0900

    チャートの作成

--- flutter_shopping/lib/main.dart
+++ flutter_shopping/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shopping/widgets/new_transaction.dart';
-import 'package:flutter_shopping/widgets/transaction_list.dart';
 
 import '../widgets/new_transaction.dart';
 import '../widgets/transaction_list.dart';
 import './models/transaction.dart';
+import '../widgets/chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -50,6 +49,16 @@ class _MyHomePageState extends State<MyHomePage> {
     //     date: DateTime.now())
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransaction.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       title: txTitle,
@@ -93,14 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHAR!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransaction),
           ],
         ),
--- flutter_shopping/lib/widgets/chart.dart
+++ flutter_shopping/lib/widgets/chart.dart
@@ -1,18 +1,55 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+import '../models/transaction.dart';
+import '/widgets/chart_bar.dart';
 
- List<Map<String, Object>> get groupedTransactionValues {
-  return
- }
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(children: <Widget>[],),
+      child: Row(
+        children: groupedTransactionValues.map((data) {
+          return ChartBar(data['day'] as String, data['amount'] as double,
+              (data['amount'] as double) / totalSpending);
+        }).toList(),
+      ),
     );
   }
 }
