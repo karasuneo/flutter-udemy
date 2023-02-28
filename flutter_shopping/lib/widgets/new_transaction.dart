8522f6fbb63ac21515b9f73c048bf54d08f64fc0
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-28 13:45:24 +0900

    モーダルのスクロールか

--- flutter_shopping/lib/widgets/new_transaction.dart
+++ flutter_shopping/lib/widgets/new_transaction.dart
@@ -55,57 +55,64 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(labelText: 'Title'),
-                  controller: _titleController,
-                  onSubmitted: (_) => _submitData(),
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Amount'),
-                  controller: _amountController,
-                  keyboardType: TextInputType.number,
-                  onSubmitted: (_) => _submitData(),
-                ),
-                Container(
-                  height: 70,
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Text(
-                          _selectedDate == null
-                              ? 'No Date Chosen!'
-                              : 'Pocked Date: ${DateFormat.yMd().format(_selectedDate!)}',
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+            padding: EdgeInsets.only(
+              top: 10,
+              left: 10,
+              right: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+            ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: _titleController,
+                    onSubmitted: (_) => _submitData(),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    onSubmitted: (_) => _submitData(),
+                  ),
+                  Container(
+                    height: 70,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            _selectedDate == null
+                                ? 'No Date Chosen!'
+                                : 'Pocked Date: ${DateFormat.yMd().format(_selectedDate!)}',
+                          ),
                         ),
-                      ),
-                      TextButton(
-                          onPressed: _presentDatePicker,
-                          child: const Text(
-                            'Choose Date!',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ))
-                    ],
+                        TextButton(
+                            onPressed: _presentDatePicker,
+                            child: const Text(
+                              'Choose Date!',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ))
+                      ],
+                    ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: _submitData,
-                  child: Text('Add Transaction'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        Theme.of(context).colorScheme.primary, // background
-                    foregroundColor: Theme.of(context)
-                        .textTheme
-                        .labelLarge
-                        ?.color, // foreground
-                  ),
-                )
-              ])),
+                  ElevatedButton(
+                    onPressed: _submitData,
+                    child: Text('Add Transaction'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Theme.of(context).colorScheme.primary, // background
+                      foregroundColor: Theme.of(context)
+                          .textTheme
+                          .labelLarge
+                          ?.color, // foreground
+                    ),
+                  )
+                ])),
+      ),
     );
   }
 }
