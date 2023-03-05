52a6f90c09ebcc93362bf74695fb2e4c821ae171
Yusei Mizutani <yusei4520030401@gmail.com>
2023-03-05 23:04:34 +0900

    ダミーデータ

--- flutter_food/lib/category_meals_screen.dart
+++ flutter_food/lib/category_meals_screen.dart
@@ -17,8 +17,10 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle as String),
       ),
-      body: const Center(
-        child: Text('The Recipes For The Category!'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+
+        },itemCount: ,
       ),
     );
   }
