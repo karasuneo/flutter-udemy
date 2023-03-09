e522805cfc2e93b19ad95506642aee597ea26a15
Yusei Mizutani <yusei4520030401@gmail.com>
2023-03-09 11:09:01 +0900

    グローバルステイト

--- flutter_shop_app/lib/main.dart
+++ flutter_shop_app/lib/main.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop_app/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
 
+import 'package:flutter_shop_app/screens/product_detail_screen.dart';
 import 'package:flutter_shop_app/screens/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
-          secondary: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        title: 'MyShop',
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      title: 'MyShop',
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
