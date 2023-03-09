6edfed7835b5c25fc19e770f2906c3175877873e
Yusei Mizutani <yusei4520030401@gmail.com>
2023-03-09 11:51:02 +0900

    関数の追加

--- flutter_shop_app/lib/providers/products.dart
+++ flutter_shop_app/lib/providers/products.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../models/product.dart';
 
 class Products with ChangeNotifier {
-  List<Product> _items = [
+  final List<Product> _items = [
     Product(
       id: 'p1',
       title: 'Red Shirt',
@@ -40,6 +40,10 @@ class Products with ChangeNotifier {
 
   List<Product> get items {
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
   }
 
   void addProduct() {
