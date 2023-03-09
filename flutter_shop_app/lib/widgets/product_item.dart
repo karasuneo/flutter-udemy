2c31d8eb2c97664dbc6e59ffbcac10493587704f
Yusei Mizutani <yusei4520030401@gmail.com>
2023-03-09 14:29:00 +0900

    お気に入りボタン追加

--- flutter_shop_app/lib/widgets/product_item.dart
+++ flutter_shop_app/lib/widgets/product_item.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_shop_app/providers/product.dart';
 import 'package:flutter_shop_app/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  const ProductItem(this.id, this.title, this.imageUrl);
+  // const ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GestureDetector(
         onTap: () {
-          Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id);
+          Navigator.of(context).pushNamed(
+            ProductDetailScreen.routeName,
+            arguments: product.id,
+          );
         },
         child: GridTile(
-          child: Image.network(
-            imageUrl,
-            fit: BoxFit.cover,
-          ),
           footer: GridTileBar(
             backgroundColor: Colors.black87,
             leading: IconButton(
-              icon: Icon(Icons.favorite),
-              onPressed: () {},
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
               color: Theme.of(context).colorScheme.secondary,
             ),
             title: Text(
-              title,
+              product.title,
               textAlign: TextAlign.center,
             ),
             trailing: IconButton(
@@ -37,6 +44,10 @@ class ProductItem extends StatelessWidget {
               onPressed: () {},
               color: Theme.of(context).colorScheme.secondary,
             ),
+          ),
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
         ),
       ),
